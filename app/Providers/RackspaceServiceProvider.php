95724e035efd0d2ffbfe678c0c1819391e278f8a
Phillip Madsen <phillip.madsen@codecorp.com>
2020-10-26 17:24:10 -0600

    Revert "Update RackspaceServiceProvider.php"
    This reverts commit c604312185eb55e4f9ea96944a3943ab6f2737d7.

--- app/Providers/RackspaceServiceProvider.php
+++ app/Providers/RackspaceServiceProvider.php
@@ -24,13 +24,6 @@ class RackspaceServiceProvider extends ServiceProvider
     /**
      * Bootstrap services.
      *
-     * env('RS_USER')
-     * env('RS_API_KEY')
-     * env('RS_CONTAINER')
-     * env('RS_REGION')
-     * env('RS_URL_TYPE')
-     * env('RS_TENANTID')
-     *
      * @return void
      */
     public function boot()
@@ -39,23 +32,14 @@ class RackspaceServiceProvider extends ServiceProvider
             // $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
             //     $config['rackspace']
             // ));
-            // $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
-            //     'username' => 'kevinmonta08',
-            //     'apiKey' => '3e96591b0ec140e5b17868126f290d33',
-            // ));
-
-            // https://php-opencloud.readthedocs.io/en/latest/services/networking/index.html
-
-            $client = new OpenCloud\OpenStack('{keystoneUrl}', array(
-              'username' => env('RS_USER'),
-              'password' => env('RS_API_KEY'),
-              'tenantId' => env('RS_TENANTID'),
+            $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+                'username' => 'kevinmonta08',
+                'apiKey' => '3e96591b0ec140e5b17868126f290d33',
             ));
-
-            $store = $client->objectStoreService(env('RS_CONTAINER'), env('RS_REGION'), env('RS_URL_TYPE'));
-
+            
+            $store = $client->objectStoreService('cloudFiles', 'LON');
             $container = $store->getContainer('flysystem');
-
+            
             return new Filesystem(new Adapter($container));
         });
     }
